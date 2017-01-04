bbbe7d6c0dd2392f277a69a5b065555c2e26fd07
A.D. Slaton <ad.slaton@turner.com>
2017-01-04 10:05:18 -0500

    add dynamic day

--- public/js/time.js
+++ public/js/time.js
@@ -9,9 +9,13 @@ function checkTime(i) {
 }
 (function startTime() {
     var today = new Date(),
+        y = today.getFullYear(),
+        mo = today.getMonth(),
+        d = today.getDay(),
         h = today.getHours(),
-        m = today.getMinutes(),
+        mi = today.getMinutes(),
         s = today.getSeconds(),
+        day = Math.round((new Date().setHours(23) - new Date(new Date().getYear()+1900, 0, 1, 0, 0, 0))/1000/60/60/24),
         t,
         part = 'AM';
 
@@ -22,10 +26,72 @@ function checkTime(i) {
         }
     }
 
-    m = checkTime(m);
+    mi = checkTime(mi);
     s = checkTime(s);
 
-    document.getElementById('clock').innerHTML = h + ':' + m + ':' + s + ' ' + part + ' EST';
+    switch (mo) {
+        case 0:
+            mo = 'Jan';
+            break;
+        case 1:
+            mo = 'Feb';
+            break;
+        case 2:
+            mo = 'Mar';
+            break;
+        case 3:
+            mo = 'Apr';
+            break;
+        case 4:
+            mo = 'May';
+            break;
+        case 5:
+            mo = 'Jun';
+            break;
+        case 6:
+            mo = 'Jul';
+            break;
+        case 7:
+            mo = 'Aug';
+            break;
+        case 8:
+            mo = 'Sep';
+            break;
+        case 9:
+            mo = 'Oct';
+            break;
+        case 10:
+            mo = 'Nov';
+            break;
+        default:
+            mo = 'Dec';
+    }
+
+    switch (d) {
+        case 0:
+            d = 'Sunday';
+            break;
+        case 1:
+            d = 'Monday';
+            break;
+        case 2:
+            d = 'Tuesday';
+            break;
+        case 3:
+            d = 'Wednesday';
+            break;
+        case 4:
+            d = 'Thursday';
+            break;
+        case 5:
+            d = 'Friday';
+            break;
+        default:
+            d = 'Saturday';
+    }
+
+    document.getElementById('day').innerHTML = d + ' ' + mo + ' ' + day + ', ' + y;
+    document.getElementById('clock').innerHTML = h + ':' + mi + ':' + s + ' ' + part + ' EST';
 
     t = setTimeout(startTime, 500);
 })();
